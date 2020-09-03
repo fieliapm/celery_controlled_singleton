1461a082827d28fbb160ffe1fee6c8f7e4840b03
fieliapm <fieliapm@gmail.com>
2020-09-04 06:20:46 +0800

    rewrite functions structure

--- celery_controlled_singleton.py
+++ celery_controlled_singleton.py
@@ -28,7 +28,9 @@ INSPECT_DELAY = 0.1
 
 
 # celery tasks inspect
-# CAUTION: if no active task can pass task_filter_func(task), we just assume: current task is the only one which is running
+
+# CAUTION: if no active task has self task name and can pass task_filter_func(args, kwargs),
+# we just assume: self task is the only one which is running
 
 
 def __inspect_active_task_list(celery_app, timeout=None):
@@ -37,59 +39,69 @@ def __inspect_active_task_list(celery_app, timeout=None):
     kwargs = {}
     if timeout is not None:
         kwargs['timeout'] = timeout
-    task_list = celery_app.control.inspect(**kwargs)
-    return task_list.active()
+    inspect_obj = celery_app.control.inspect(**kwargs)
+    return inspect_obj.active()
 
 
-def __filter_task(task_filter_func, task):
-    return task_filter_func(task['name'], task['args'], task['kwargs'])
+def iter_task_list(task_list):
+    for (node, task_status_list) in task_list.items():
+        for task_status in task_status_list:
+            yield task_status
 
 
-def __find_current_task(current_task_id, task_list):
-    for (node, tasks) in task_list.items():
-        for task in tasks:
-            if task['id'] == current_task_id:
-                return task
+def __filter_task(task_status, self_task_name, task_filter_func):
+    #return task_status['name'] == self_task_name and task_filter_func is None or task_filter_func(task_status['args'], task_status['kwargs'])
+    if task_status['name'] == self_task_name:
+        if task_filter_func is None:
+            return True
+        else:
+            return task_filter_func(task_status['args'], task_status['kwargs'])
+    else:
+        return False
+
+
+def __find_task_status(task_list, task_id):
+    for task_status in iter_task_list(task_list):
+        if task_status['id'] == task_id:
+            return task_status
     return None
 
 
-def __find_current_task_start_rank(current_task, task_filter_func, task_list):
+def __find_task_start_rank(task_list, self_task_status, task_filter_func):
     rank = 0
-    for (node, tasks) in task_list.items():
-        for task in tasks:
-            if ___filter_task(task_filter_func, task):
-                if task['time_start'] <= current_task['time_start']:
-                    rank += 1
+    for task_status in iter_task_list(task_list):
+        if __filter_task(task_status, self_task_status['name'], task_filter_func):
+            if task_status['time_start'] <= self_task_status['time_start']:
+                rank += 1
     return rank
 
 
-def get_task_start_rank(celery_app, current_task_id, task_filter_func, timeout=None):
+def get_task_start_rank(celery_app, self, task_filter_func=None, timeout=None):
     active_task_list = __inspect_active_task_list(celery_app, timeout)
     if active_task_list is None:
         return 0
 
-    current_task = __find_current_task(current_task_id, active_task_list)
-    if current_task is None:
+    self_task_status = __find_task_status(active_task_list, self.request.id)
+    if self_task_status is None:
         return 0
 
-    return __find_current_task_start_rank(current_task, task_filter_func, active_task_list)
+    return __find_task_start_rank(active_task_list, self_task_status, task_filter_func)
 
 
-def __find_first_started_task(task_filter_func, task_list):
-    first_started_task = None
-    for (node, tasks) in task_list.items():
-        for task in tasks:
-            if __filter_task(task_filter_func, task):
-                if first_started_task is None or task['time_start'] < first_started_task['time_start']:
-                    first_started_task = task
-    return first_started_task
+def __find_first_started_task_status(task_list, task_name, task_filter_func):
+    first_started_task_status = None
+    for task_status in iter_task_list(task_list):
+        if __filter_task(task_status, task_name, task_filter_func):
+            if first_started_task_status is None or task_status['time_start'] < first_started_task_status['time_start']:
+                first_started_task_status = task_status
+    return first_started_task_status
 
 
-def is_first_started_task(celery_app, current_task_id, task_filter_func, timeout=None):
+def is_first_started_task(celery_app, self, task_filter_func=None, timeout=None):
     active_task_list = __inspect_active_task_list(celery_app, timeout)
     if active_task_list is None:
         return True
 
-    first_started_task = __find_first_started_task(task_filter_func, active_task_list)
-    return first_started_task is None or first_started_task['id'] == current_task_id
+    first_started_task_status = __find_first_started_task_status(active_task_list, self.name, task_filter_func)
+    return first_started_task_status is None or first_started_task_status['id'] == self.request.id
 
